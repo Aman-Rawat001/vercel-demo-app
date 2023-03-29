ba96e5b9a3f660bc94e163b6ea70c6004538f565
Aman Rawat <amanrawa2@gmail.com>
2023-03-29 14:19:34 +0530

    page added

---
+++ src/components/Homepage.js
@@ -0,0 +1,83 @@
+import React from "react";
+import img7 from "../components/Images/images7.jpg";
+import img10 from "../components/Images/images10.jpg";
+import img5 from "../components/Images/images5.jpg";
+import img6 from "../components/Images/images6.jpg";
+import img9 from "../components/Images/images9.jpg";
+import img8 from "../components/Images/images8.jpg";
+import img4 from "../components/Images/images4.jpg";
+// import img7 from "../components/Images/images7.jpg";
+
+const HomePage = () => {
+  return (
+    <>
+      <div className="container">
+        <h1>Cristiano Ronaldo & Georgina Rodriguez's Family Album</h1>
+        <img src={img7} />
+        <div>
+          <h2>New Arrival</h2>
+          <p>
+            Cristiano shared their first family photo after welcoming their baby
+            girl in April 2022. He announced the tragic death of the baby's twin
+            brother a few days earlier.
+          </p>
+        </div>
+        <img src={img10} />
+        <div>
+          <h2>Babies on Board</h2>
+          <p>
+            On Oct. 28, 2021, Cristiano and Georgina shared their pregnancy
+            announcement with the world.
+          </p>
+        </div>
+        <img src={img8} />
+        <div>
+          <p>
+            Cristiano Ronaldo's girlfriend Georgina Rodriguez posted pictures of
+            their trip to the Saudi Arabian desert with their children on
+            Instagram. The Al Nassr forward was enjoying some Middle Eastern sun
+            before his team's clash with Al Wehda. In this collection of
+            pictures, you can see Ronaldo and his family enjoying the Saudi
+            heat.
+          </p>
+        </div>
+        <img src={img4} />
+        <div>
+          <h1>
+            Cristiano Ronaldo: 'I'm the Best Player in History,' 'Have Always
+            Thought That'
+          </h1>
+          <p>
+            Real Madrid forward Cristiano Ronaldo has said that he is the "best
+            player in history" after winning his fifth Ballon d'Or on Thursday.
+            Ronaldo picked up the award for the second year in a row to equal
+            the record of Barcelona star Lionel Messi, and he said he does not
+            believe any player is better than him. He told France Football (h/t
+            Goal's Robin Bairner): "I've never seen anyone better than me. I
+            have always thought that. No footballer can do the things I can.
+            "There’s no player more complete than me. I play well with both
+            feet, I’m quick, powerful, good with the head, I score goals, I make
+            assists. There are guys who prefer Neymar or Messi. But I tell you:
+            there’s no one more complete than me.
+          </p>
+          <img src={img5} />
+          <p>
+            "There’s no player more complete than me. I play well with both
+            feet, I’m quick, powerful, good with the head, I score goals, I make
+            assists. There are guys who prefer Neymar or Messi. But I tell you:
+            there’s no one more complete than me. Ronaldo enjoyed a superb 2017,
+            winning the Liga title as well as the UEFA Champions League.
+            Madrid's victory over Juventus in the final ensured Los Blancos
+            became the first club ever to retain the trophy in its modern
+            format. Ronaldo scored twice in the final, proving once again he is
+            a man for the big occasion, and it is these achievements that saw
+            him win the Ballon d'Or ahead of great rival Messi. Opta also showed
+            how he set yet another record in Europe on Wednesday:
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
